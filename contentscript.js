c3176a9f24ecb41d78e91c7fdc15c822454ebec4
Pudding <philip.strecker@googlemail.com>
2017-09-16 23:22:43 +0200

    added material design. added disable/enable feature for each separate website

--- contentscript.js
+++ contentscript.js
@@ -1,20 +1,37 @@
-var STORAGE_KEY = "video-volume";
+(function() {
+    const STORAGE_KEY = "video-volume";
+    const STATUS_KEY = "video-volume-status-per-website";
 
-function setVolume() {
-    var videoTags = document.getElementsByTagName("video");
-    if (videoTags.length == 0) {
-        return;
+    function setVolume() {
+        let host = window.location.host;
+        let key = STATUS_KEY + '_' + host;
+        chrome.storage.local.get(key, function(data) {
+            if (key in data && !data[key]) {
+                return;
+            }
+            var videoTags = document.getElementsByTagName("video");
+            if (videoTags.length == 0) {
+                return;
+            }
+
+            chrome.storage.local.get(STORAGE_KEY, function(data) {
+                if (!data || !(STORAGE_KEY in data)) {
+                    return;
+                }
+                for (var x = 0; x < videoTags.length; x++) {
+                    videoTags[x].volume = data[STORAGE_KEY] / 100;
+                }
+            });    
+        });
     }
 
-    chrome.storage.local.get(STORAGE_KEY, function(data) {
-        if (!data || !(STORAGE_KEY in data)) {
-            return;
-        }
-        for (var x = 0; x < videoTags.length; x++) {
-            videoTags[x].volume = data[STORAGE_KEY] / 100;
-        }
-    });    
-}
+    function parseHostFromURL(url) {
+        var parser = document.createElement('a');
+        parser.href = url;
+        return parser.host;
+    }
 
-setVolume();
-window.setInterval(setVolume, 1000);
+    setVolume();
+    window.setInterval(setVolume, 1000);    
+})();
+
--- popup.js
+++ popup.js
@@ -1,31 +1,63 @@
-var STORAGE_KEY = "video-volume";
+const STORAGE_KEY = "video-volume";
+const STATUS_KEY = "video-volume-status-per-website";
 
 document.addEventListener('DOMContentLoaded', function() {
-    var text = document.getElementById("volume-text");
-    var element = document.getElementById("volume");  
+    const slider = new mdc.slider.MDCSlider(document.querySelector('.mdc-slider'));
+
+    const status = document.getElementById('status');
+    mdc.checkbox.MDCCheckbox.attachTo(document.querySelector('.mdc-checkbox'));
+
+    var text = document.getElementById("volume-value-label");
 
     var setLabel = function(volume) {
         text.innerHTML = volume + '%';
     }
 
-    element.addEventListener("change", function(e) {
-        var volume = e.target.value;
+    slider.listen('MDCSlider:change', () => {
         var key = {};
-        key[STORAGE_KEY] = volume;
+        key[STORAGE_KEY] = slider.value;
         chrome.storage.local.set(key, function() {
-            setLabel(volume);
+            setLabel(slider.value);
         });
-    });    
-
+    });
+    status.addEventListener('change', () => {
+        let enabled = status.checked;
+        slider.disabled = !enabled;
+        chrome.tabs.query({active: true}, function(tabs) {
+            let host = parseHostFromURL(tabs[0].url);
+            let key = {};
+            key[STATUS_KEY + '_' + host] = enabled;
+            chrome.storage.local.set(key);
+        });
+    });
+    
+    chrome.tabs.query({active: true}, function(tabs) {
+        let host = parseHostFromURL(tabs[0].url);
+        let key = STATUS_KEY + '_' + host;
+        chrome.storage.local.get(key, function(data) {
+            if (key in data) {
+                slider.disabled = !data[key];
+                status.checked = data[key];
+            } else {
+                status.checked = true;
+            }
+        });    
+    });
+    
     chrome.storage.local.get(STORAGE_KEY, function(data) {
         var volume = data[STORAGE_KEY];
         if (!volume) {
             volume = 0;
         }
-        element.value = volume;
+        slider.value = volume;
         setLabel(volume);
     });
 
-
+    function parseHostFromURL(url) {
+        var parser = document.createElement('a');
+        parser.href = url;
+        return parser.host;
+    }
 
 });
+

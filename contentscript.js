6971818f3296c8911ba4fad2333cc80e693c12cc
Pudding <philip.strecker@googlemail.com>
2018-08-19 00:46:32 +0200

    added support for 'native' volume control changes

--- contentscript.js
+++ contentscript.js
@@ -3,6 +3,7 @@
     const STATUS_KEY = "video-volume-status-per-website";
 
     let store = chrome.storage || window.storage;
+    lock = false;
     function setVolume() {
         let host = window.location.host;
         let key = STATUS_KEY + '_' + host;
@@ -16,18 +17,42 @@
             
             if (videoTags.length == 0 && audioTags.length == 0) {
                 return;
-            }            
+            }
 
             store.local.get(STORAGE_KEY, function(data) {
                 if (!data || !(STORAGE_KEY in data)) {
                     return;
                 }
+                if (lock) {
+                    return;
+                }
+
                 for (var x = 0; x < videoTags.length; x++) {
                     videoTags[x].volume = data[STORAGE_KEY] / 100;
                 }
                 for (var x = 0; x < audioTags.length; x++) {
                     audioTags[x].volume = data[STORAGE_KEY] / 100;
                 }
+
+                for (var i in videoTags) {
+                    videoTags[i].onvolumechange = function (argument) {
+                        lock = true;
+                        var key = {};
+                        key[STORAGE_KEY] = Math.round(argument.target.volume * 100)
+                        store.local.set(key, function() {
+                            lock = false;
+                        });
+                    }
+                    audioTags[i].onvolumechange = function (argument) {
+                        lock = true;
+                        var key = {};
+                        key[STORAGE_KEY] = Math.round(argument.target.volume * 100)
+                        store.local.set(key, function() {
+                            lock = false;
+                        });
+                    }
+                }
+
             });
         });
     }
@@ -39,5 +64,5 @@
     }
 
     setVolume();
-    window.setInterval(setVolume, 1000);
+    window.setInterval(setVolume, 100);
 })();

b9a336873eebace0115f59c89e466b0c27599425
Pudding <philip.strecker@googlemail.com>
2018-08-26 03:23:54 +0200

    Changed volume control to be website based. Added max global volume control.

--- contentscript.js
+++ contentscript.js
@@ -1,5 +1,6 @@
 (function() {
     const STORAGE_KEY = "video-volume";
+    const MAX_VOL_STORAGE_KEY = "max-global-video-volume";
     const STATUS_KEY = "video-volume-status-per-website";
 
     let store = chrome.storage || window.storage;
@@ -8,10 +9,8 @@
     function setVolume() {
         let host = window.location.host;
         let key = STATUS_KEY + '_' + host;
-        store.local.get(key, function(data) {
-            if (key in data && !data[key]) {
-                return;
-            }
+        let websiteKey = STORAGE_KEY + '_' + host;
+        store.local.get([key, MAX_VOL_STORAGE_KEY, websiteKey, STORAGE_KEY], function(data) {
 
             var videoTags = document.getElementsByTagName("video");
             var audioTags = document.getElementsByTagName("audio");
@@ -20,49 +19,60 @@
                 return;
             }
 
-            store.local.get(STORAGE_KEY, function(data) {
-                if (!data || !(STORAGE_KEY in data)) {
+            if (lock) {
+                return;
+            }
+
+            let maxVol = data[MAX_VOL_STORAGE_KEY] || 0.5;
+
+            for (var x = 0; x < videoTags.length; x++) {
+                if (websiteKey in data && key in data && data[key] === true) {
+                    videoTags[x].volume = data[websiteKey] / 100;
+                } else if (maxVol / 100 < videoTags[x].volume) {
+                    videoTags[x].volume = maxVol / 100;
+                }
+            }
+            for (var x = 0; x < audioTags.length; x++) {
+                if (websiteKey in data && key in data && data[key] === true) {
+                    audioTags[x].volume = data[websiteKey] / 100;
+                } else if (maxVol / 100 < audioTags[x].volume) {
+                    audioTags[x].volume = maxVol / 100;
+                }
+            }
+
+            for (var i in videoTags) {
+                if (!videoTags[i]) {
                     return;
                 }
-                if (lock) {
+                if (!(key in data) || !data[key]) {
                     return;
                 }
+                videoTags[i].onvolumechange = function (argument) {
+                    lock = true;
+                    var key = {};
+                    key[STORAGE_KEY + '_' + host] = Math.round(argument.target.volume * 100)
+                    store.local.set(key, function() {
+                        lock = false;
+                    });
+                }
+            }
+            for (var i in audioTags) {
+                if (!audioTags[i]) {
+                    return;
+                }
+                if (!(key in data) || !data[key]) {
+                    return;
+                }
+                audioTags[i].onvolumechange = function (argument) {
+                    lock = true;
+                    var key = {};
+                    key[STORAGE_KEY + '_' + host] = Math.round(argument.target.volume * 100)
+                    store.local.set(key, function() {
+                        lock = false;
+                    });
+                }
+            }
 
-                for (var x = 0; x < videoTags.length; x++) {
-                    videoTags[x].volume = data[STORAGE_KEY] / 100;
-                }
-                for (var x = 0; x < audioTags.length; x++) {
-                    audioTags[x].volume = data[STORAGE_KEY] / 100;
-                }
-
-                for (var i in videoTags) {
-                    if (!videoTags[i]) {
-                        return;
-                    }
-                    videoTags[i].onvolumechange = function (argument) {
-                        lock = true;
-                        var key = {};
-                        key[STORAGE_KEY] = Math.round(argument.target.volume * 100)
-                        store.local.set(key, function() {
-                            lock = false;
-                        });
-                    }
-                }
-                for (var i in audioTags) {
-                    if (!audioTags[i]) {
-                        return;
-                    }
-                    audioTags[i].onvolumechange = function (argument) {
-                        lock = true;
-                        var key = {};
-                        key[STORAGE_KEY] = Math.round(argument.target.volume * 100)
-                        store.local.set(key, function() {
-                            lock = false;
-                        });
-                    }
-                }
-
-            });
         });
     }
 
